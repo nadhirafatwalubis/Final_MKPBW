77203bdf7f4aac86273161ac36648f58579787c4
AjetAzza <101322510+AjetAzza@users.noreply.github.com>
2022-05-22 13:07:26 +0700

    Add files via upload

---
+++ config/myblog.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'uploadPath' => [
+        'setting'  => 'images'
+    ],
+    'default_category_id' => 1,
+    'thumbnail'           => [
+        'width'  => 400,
+        'height' => 170,
+    ]
+];
